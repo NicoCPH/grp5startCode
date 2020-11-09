4cd5e051fa38671054f42cc9f4c101ef7f354a49
A-R-G-Wagner <cph-aw116@cphbusiness.dk>
2020-11-09 20:21:36 +0100

    first commit

---
+++ Grp5startCode/src/main/java/rest/UserResource.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rest;
+
+import javax.annotation.security.RolesAllowed;
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
+import utils.EMF_Creator;
+
+/**
+ * REST Web Service
+ *
+ * @author GRP 5
+ */
+@Path("user")
+public class UserResource {
+    
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
+
+    @Context
+    private UriInfo context;
+    
+    @Context
+    SecurityContext securityContext;
+
+    /**
+     * Creates a new instance of UserResource
+     */
+    public UserResource() {
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("user")
+    @RolesAllowed("user")
+    public String getFromUser() {
+        String thisuser = securityContext.getUserPrincipal().getName();
+        return "{\"msg\": \"Hello to User: " + thisuser + "\"}";
+    }
+    
+}
