2155042e6c21ed109f712830376ca8e9c8cd2676
Lange <cph-na157@cphbusiness.dk>
2020-11-10 12:59:55 +0100

    oprydning

--- Grp5startCode/src/main/java/rest/ApplicationConfig.java
+++ Grp5startCode/src/main/java/rest/ApplicationConfig.java
@@ -25,7 +25,6 @@ public class ApplicationConfig extends Application {
         resources.add(errorhandling.GenericExceptionMapper.class);
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
         resources.add(rest.AdminResource.class);
-        resources.add(rest.DemoResource.class);
         resources.add(rest.RenameMeResource.class);
         resources.add(rest.UserResource.class);
         resources.add(security.JWTAuthenticationFilter.class);
